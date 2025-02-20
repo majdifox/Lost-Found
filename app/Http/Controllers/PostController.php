90f4077baab9ce62797476b900555ed2a96fbb3e
majmehdi <majmehdi20@gmail.com>
2025-02-21 00:26:44 +0100

    Fixing the create method, adding the model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -13,5 +14,12 @@ class PostController extends Controller
             'title' => 'required',
             'body' => 'required'
         ]);
+
+        $inscomingFields['title'] = strip_tags($inscomingFields['title']);
+        $inscomingFields['body'] = strip_tags($inscomingFields['body']);
+        $inscomingFields['user_id'] = auth()->id();
+        Post::create($inscomingFields); 
+        return redirect('/');
+
     }
 }
